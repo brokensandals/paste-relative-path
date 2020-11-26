a7542318481d1b3140908cb911a2a3bd8c000304
Jacob Williams <jacobaw@gmail.com>
2020-11-26 01:38:21 -0800

    code refactoring

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to the "paste-relative-path" extension will be documented in
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
+## Unreleased
+
+- Refactoring
+
 ## 1.1.0
 
 - Resolve symlinks before calculating relative path
--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,50 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as util from 'util';
+
+const realpath = util.promisify(fs.realpath);
+
+function hrefRelPath(src: string, dest: string) {
+  const relPath = path.relative(src, dest);
+  if (relPath === '') {
+    // The path on the clipboard is the same file we're pasting into.
+    return '.';
+  } else if (relPath.startsWith('..' + path.sep)) {
+    // Given source path "/a/b/c.md" and dest path "/a/b/d.md",
+    // path.relative returns "../d.md". But generally you really want
+    // the path relative to the directory _containing_ c.md - that's
+    // what you would put in a hyperlink href, for example. So, we
+    // remove the first '..' path component.
+    return relPath.slice(3);
+  } else {
+    return relPath;
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('paste-relative-path.paste', () => {
-    vscode.env.clipboard.readText().then((value: string) => {
-      const active = vscode.window.activeTextEditor;
-      if (!active) {
-        return;
-      }
+  let disposable = vscode.commands.registerCommand('paste-relative-path.paste', () => {(async () => {
+    const rawDest = await vscode.env.clipboard.readText();
+    const realDest = await realpath(rawDest);
+    const rawSrc = vscode.window.activeTextEditor?.document?.fileName;
+    if (!rawSrc) {
+      return;
+    }
+    const realSrc = await realpath(rawSrc);
+    const href = hrefRelPath(realSrc, realDest);
 
-      fs.realpath(value, (err, realTargetPath) => {
-        if (err) {
-          vscode.window.showErrorMessage(`Error converting ${value} to real path: ${err}`);
-          return;
+    await vscode.window.activeTextEditor?.edit((eb) => {
+      vscode.window.activeTextEditor?.selections?.forEach((selection) => {
+        if (selection.isEmpty) {
+          eb.insert(selection.active, href);
+        } else {
+          eb.replace(selection, href);
         }
-
-        const activePath = active.document.fileName;
-        fs.realpath(activePath, (err, realActivePath) => {
-          if (err) {
-            vscode.window.showErrorMessage(`Error converting ${activePath} to real path: ${err}`);
-            return;
-          }
-
-          const relPath = path.relative(realActivePath, realTargetPath);
-          let finalPath: string;
-          if (relPath === '') {
-            // The path on the clipboard is the same file we're pasting into.
-            finalPath = '.';
-          } else if (relPath.startsWith('..' + path.sep)) {
-            // Given source path "/a/b/c.md" and dest path "/a/b/d.md",
-            // path.relative returns "../d.md". But generally you really want
-            // the path relative to the directory _containing_ c.md - that's
-            // what you would put in a hyperlink href, for example. So, we
-            // remove the first '..' path component.
-            finalPath = relPath.slice(3);
-          } else {
-            finalPath = relPath;
-          }
-
-          active.edit((eb: vscode.TextEditorEdit) => {
-            active.selections.forEach((selection: vscode.Selection) => {
-              if (selection.isEmpty) {
-                eb.insert(selection.active, finalPath);
-              } else {
-                eb.replace(selection, finalPath);
-              }
-            });
-          });
-        });
       });
     });
-  });
-
+  })().catch((e) => {
+    vscode.window.showErrorMessage(`${e}`);
+  })});
   context.subscriptions.push(disposable);
 }
 

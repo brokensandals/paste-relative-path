cc7ca8fd0bf283aa9922da00d8027a60e657095a
Jacob Williams <jacobaw@gmail.com>
2020-11-26 02:10:54 -0800

    Start work on paste-markdown command. All that is missing to try to read the title from the file.

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,9 @@ import * as path from 'path';
 import * as util from 'util';
 
 const realpath = util.promisify(fs.realpath);
+const readFile = util.promisify(fs.readFile);
 
-function hrefRelPath(src: string, dest: string) {
+function refPath(src: string, dest: string) {
   const relPath = path.relative(src, dest);
   if (relPath === '') {
     // The path on the clipboard is the same file we're pasting into.
@@ -22,6 +23,15 @@ function hrefRelPath(src: string, dest: string) {
   }
 }
 
+async function getTitle(filePath: string) {
+  try {
+    const text = await readFile(filePath, 'utf8');
+    // TODO
+  } catch (e) {
+    return null;
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('paste-relative-path.paste', () => {(async () => {
     const rawDest = await vscode.env.clipboard.readText();
@@ -31,14 +41,41 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
     const realSrc = await realpath(rawSrc);
-    const href = hrefRelPath(realSrc, realDest);
+    const ref = refPath(realSrc, realDest);
 
     await vscode.window.activeTextEditor?.edit((eb) => {
       vscode.window.activeTextEditor?.selections?.forEach((selection) => {
         if (selection.isEmpty) {
-          eb.insert(selection.active, href);
+          eb.insert(selection.active, ref);
         } else {
-          eb.replace(selection, href);
+          eb.replace(selection, ref);
+        }
+      });
+    });
+  })().catch((e) => {
+    vscode.window.showErrorMessage(`${e}`);
+  })});
+  context.subscriptions.push(disposable);
+
+  disposable = vscode.commands.registerCommand('paste-relative-path.paste-markdown', () => {(async () => {
+    const rawDest = await vscode.env.clipboard.readText();
+    const realDest = await realpath(rawDest);
+    const rawSrc = vscode.window.activeTextEditor?.document?.fileName;
+    if (!rawSrc) {
+      return;
+    }
+    const realSrc = await realpath(rawSrc);
+    const href = encodeURIComponent(refPath(realSrc, realDest));
+
+    let title = path.basename(realDest).replace(/\..*/, '');
+
+    await vscode.window.activeTextEditor?.edit((eb) => {
+      vscode.window.activeTextEditor?.selections?.forEach((selection) => {
+        if (selection.isEmpty) {
+          eb.insert(selection.active, `[${title}](${href})`);
+        } else {
+          const text = vscode.window.activeTextEditor?.document.getText(selection);
+          eb.replace(selection, `[${text}](${href})`);
         }
       });
     });
